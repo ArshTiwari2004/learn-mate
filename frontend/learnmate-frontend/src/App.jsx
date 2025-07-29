1964a7574a95aaf3ab2eda9f2357fbbb60705553
Priyanshi Bothra <priyanshibothra753@gmail.com>
2025-07-29 23:09:32 +0530

    frontend added

---
+++ frontend/learnmate-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-black p-10 rounded-lg shadow text-center">
+        <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind!</h1>
+        <p className="text-gray-700 mt-2">Tailwind CSS is working in React 🎉</p>
+        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Click Me</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
